d29dcb7014aaeb9c87e25329741de29405aa5a31
M Venkat Naidu <info@mvnaidu.in>
2019-12-28 12:11:53 +0530

    Init Java Project with a Package and Hello.java

---
+++ src/master/Hello.java
@@ -0,0 +1,12 @@
+package master;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello From Jenkins");
+		System.out.println("If you are seeing this means it's working");
+
+	}
+
+}
